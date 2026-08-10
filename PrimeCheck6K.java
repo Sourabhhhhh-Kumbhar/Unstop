1c322e516575d1a033ea7c52e8e8cc994607fc91
Sourabhhhhh <sourabhkumbhar787571@gmail.com>
2026-08-10 19:24:41 +0530

    Mathematical Optimization 6k _+ Method

---
+++ PrimeCheck6K.java
@@ -0,0 +1,27 @@
+class Prime6k {
+    static boolean isPrime(int n) {
+        if (n <= 1)
+            return false;
+
+        if (n == 2 || n == 3)
+            return true;
+
+        if (n % 2 == 0 || n % 3 == 0)
+            return false;
+
+        for (int i = 5; i * i <= n; i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0)
+                return false;
+        }
+
+        return true;
+    }
+}
+
+public class PrimeCheck6K {
+    public static void main(String[] args) {
+        System.out.println(Prime6k.isPrime(6));
+        System.out.println(Prime6k.isPrime(11));
+        System.out.println(Prime6k.isPrime(121));
+    }
+}
