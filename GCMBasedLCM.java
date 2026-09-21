042d876a48b7dab428a0ce60fcee03169aa38bb6
Sourabhhhhh <sourabhkumbhar787571@gmail.com>
2026-09-21 19:32:16 +0530

    LCM based on GCM

--- GCMBasedLCM.java
+++ GCMBasedLCM.java
@@ -1,22 +1,40 @@
+// Program to find LCM using GCD
 public class GCMBasedLCM
 {
-    public static int gcd (int a, int b)
+    // Method to calculate GCD (Greatest Common Divisor)
+    public static int gcd(int a, int b)
     {
+        // Repeat until b becomes 0
         while(b != 0)
         {
+            // Store the value of b temporarily
             int temp = b;
+
+            // Find the remainder of a divided by b
             b = a % b;
+
+            // Assign old value of b to a
             a = temp;
         }
+
+        // Return the GCD
         return a;
     }
+
+    // Method to calculate LCM (Least Common Multiple)
     public static int lcm(int a, int b)
     {
-        return (a * b)/ gcd(a, b);
+        // Formula: LCM = (a * b) / GCD(a, b)
+        return (a * b) / gcd(a, b);
     }
-    public static void main(String[]args)
+
+    // Main method - program execution starts here
+    public static void main(String[] args)
     {
+        // Declare two numbers
         int a = 12, b = 18;
-        System.out.println("LCM of " + a + " and " + b + " is " + lcm(a,b));
+
+        // Calculate and display the LCM
+        System.out.println("LCM of " + a + " and " + b + " is " + lcm(a, b));
     }
 }
