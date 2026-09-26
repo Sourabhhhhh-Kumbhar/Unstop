46642749b4ba2a2faa292b8961f86ada7e47ffae
Sourabhhhhh <sourabhkumbhar787571@gmail.com>
2026-09-26 14:48:43 +0530

    Decimal to hexadecimal

---
+++ DecimalToHexadecimal.java
@@ -0,0 +1,9 @@
+public class DecimalToHexadecimal
+{
+    public static void main(String[]args)
+    {
+        int num = 50;
+        String hex = Integer.toHexString(num);
+        System.out.println(hex);
+    }
+}
